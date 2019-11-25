2b4122bcba51ec70b14b7a6139297c0237dc7c14
erik.juvan <erik.kiefer.juvan@sensum.eu>
2019-11-25 21:09:02 +0100

    clang_format

--- cycle_counting.c
+++ cycle_counting.c
@@ -1,4 +1,3 @@
-
 ////////////////////////////
 // Counting cycles using DWT
 ////////////////////////////
@@ -8,48 +7,55 @@ DWT_CONTROL  = (int *)0xE0001000; DWT->CTRL
 SCB_DEMCR    = (int *)0xE000EDFC; CoreDebug->DEMCR
 */
 
-void EnableCC() {
-	CoreDebug->DEMCR |= 0x01000000;	// Enable DWT and ITM features
-	DWT->CYCCNT = 0;	// Set Cycle Count register to zero	
-	DWT->CTRL |= 1;	// Enable CYCCNT
+void EnableCC()
+{
+    CoreDebug->DEMCR |= 0x01000000; // Enable DWT and ITM features
+    DWT->CYCCNT = 0;                // Set Cycle Count register to zero
+    DWT->CTRL |= 1;                 // Enable CYCCNT
 }
 
-void DisableCC() {
-	DWT->CTRL &= ~1;	// Disable CYCCNT
+void DisableCC()
+{
+    DWT->CTRL &= ~1; // Disable CYCCNT
 }
 
-#define RESET_CC() DWT->CYCCNT = 0	// Set Cycle Count register to zero
+#define RESET_CC() DWT->CYCCNT = 0 // Set Cycle Count register to zero
 #define GET_CC() DWT->CYCCNT
 
-#define RESET_CC_ASM() \
-	__asm__("ldr r0, =0"); \
-	__asm__("ldr r4, =0xE0001004"); \
-	__asm__("str r0, [r4]")
-	
-#define GET_CC_ASM() __asm__("ldr r0, [r4]")	// r0 = DWT->CYCCNT
+// ASM macros
+/////////////
+#define RESET_CC_ASM()              \
+    __asm__("ldr r0, =0");          \
+    __asm__("ldr r4, =0xE0001004"); \
+    __asm__("str r0, [r4]")
 
-// Example C with ASM
+#define GET_CC_ASM() __asm__("ldr r0, [r4]") // r0 = DWT->CYCCNT
+/////////////
+
+/* ******** */
+/* EXAMPLES */
+/* ******** */
+
+// Example C with asm
 /////////////////////
 EnableCC();
 
 RESET_CC_ASM();
-// Action to time
+// action_to_time();
 GET_CC_ASM();
-__asm__("nop");	// useful for breakpoint
+__asm__("nop"); // useful for breakpoint
 /////////////////////
 
-
-// Example only C:
+// Example C:
+/////////////////////
 EnableCC();
+
 ResetCC();
-// Action to time
-int cycles = GetCC();
+// action();
+int    cycles        = GetCC();
 double seconds_taken = cycles * (1.0 / (float)SystemCoreClock);
 __asm__("nop");
 /////////////////////////////////
-
-
-
 
 ////////////////////////////////
 // Counting cycles using SysTick
@@ -63,22 +69,23 @@ int *STCVR = (int *)0xE000E018;	SysTick->VAL
 #define GetSTCVR() SysTick->VAL // The number of core clock cycles taken by the operation is given by: (STCVR1 - STCVR2 - 2)
 
 // Usually not neede because HAL_Init() already initializes SysTick
-void StartST() {
-	SysTick->LOAD = SysTick_LOAD_RELOAD_Msk;
-	SysTick->VAL = 0;
-	SysTick->CTRL = 5;
+void StartST()
+{
+    SysTick->LOAD = SysTick_LOAD_RELOAD_Msk;
+    SysTick->VAL  = 0;
+    SysTick->CTRL = 5;
 }
 
-void StopST() {
-	SysTick->CTRL = 0;
+void StopST()
+{
+    SysTick->CTRL = 0;
 }
 
 // Example (no initalization needed, because HAL_Init() already initializes SysTick
 __asm__("ldr r4, =0xE000E018");
-__asm__("ldr r0, [r4]");	// r0 = SysTick->VAL;
+__asm__("ldr r0, [r4]"); // r0 = SysTick->VAL;
 // function to time
-__asm__("ldr r1, [r4]");	// r1 = SysTick->VAL;
-__asm__("sub r0, r0, r1");	// r0 = r0 - r1 -> timer counts down
+__asm__("ldr r1, [r4]");   // r1 = SysTick->VAL;
+__asm__("sub r0, r0, r1"); // r0 = r0 - r1 -> timer counts down
 
 /////////////////////////////////
-
--- optimize.c
+++ optimize.c
@@ -1,4 +1,4 @@
-__attribute__((optimize("O2"))) void foo() {
-	// do smtn
+__attribute__((optimize("O2"))) void foo()
+{
+    // do smtn
 }
-
